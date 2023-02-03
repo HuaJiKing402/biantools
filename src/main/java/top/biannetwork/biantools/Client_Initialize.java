083ef4aceb91d13dac644cf5817b57803793eb2e
HuaJiKing <3550514081@qq.com>
2023-02-03 17:23:58 +0800

    update

--- src/main/java/top/biannetwork/biantools/Client_Initialize.java
+++ src/main/java/top/biannetwork/biantools/Client_Initialize.java
@@ -1,13 +1,26 @@
 package top.biannetwork.biantools;
 
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class Client_Initialize implements ClientModInitializer {
+    public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
+    public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool());
+
     @Override
     public void onInitializeClient() {
         Main.LOGGER.info("Biantools-Client");
-
+        Registry.register(Registries.ITEM, new Identifier("biantools", "custom_item"), CUSTOM_ITEM);
+        Registry.register(Registries.BLOCK, new Identifier("tutorial", "example_block"), EXAMPLE_BLOCK);
+        Registry.register(Registries.ITEM, new Identifier("tutorial", "example_block"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings()));
     }
 }
 
